61a814bcd4874093278fa9a9c2e142a6e214f7e2
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-12 10:11:12 +0000

    Added Category model

--- products/models.py
+++ products/models.py
@@ -1,11 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
+class Category(models.Model):
+    name         = models.CharField(max_length=100,
+                                    unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('products:category', kwargs={'name': self.name})
+
+
 class Product(models.Model):
+    category     = models.ManyToManyField(Category,
+                                          blank=False)
     name         = models.CharField(max_length=150,
                                     default='Empty name.')
     description  = models.TextField(default='Empty description.')
     price        = models.DecimalField(decimal_places=2,
                                        max_digits=20,
                                        default=0)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('products:products_detail', kwargs={'pk': self.pk})
