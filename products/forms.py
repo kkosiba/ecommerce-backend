1993b6765a0fc76c06e3d3ead3c23e41aa051bbe
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-16 11:13:59 +0000

    Implemented ProductForm

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,41 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import (
+    ModelForm,
+    TextInput,
+    Textarea,
+    SelectMultiple,
+    NumberInput, )
+
+from .models import Product
 
 class ProductForm(ModelForm):
-    pass
+    class Meta:
+        model = Product
+        exclude = ('owner', )
+        widgets = {
+            'name': TextInput(
+                attrs={
+                    'required': True,
+                    'placeholder': 'Product name',
+                    'class': 'form-control', }, ),
+            'description': Textarea(
+                attrs={
+                    'required': True,
+                    'placeholder': 'Product name',
+                    'class': 'form-control', }, ),
+            'category': SelectMultiple(
+                attrs={
+                    'required': True,
+                    'class': 'form-control', }, ),
+            'price': NumberInput(
+                attrs={
+                    'required': True, }, ),
+        }
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        if commit:
+            instance.save()
+            self.save_m2m()
+        return instance
