1d8067f5e0c3aab759fcccf99f46e21ab0a47746
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-17 10:19:20 +0000

    Changed static dirs setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,12 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 ### CUSTOM SETTINGS
