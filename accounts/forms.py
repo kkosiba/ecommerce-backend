ce275145bd707520c5f455414b115649bd848ba8
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-17 21:49:18 +0000

    Implemented custom User model according to Django documentation: https://docs.djangoproject.com/en/2.1/topics/auth/customizing/

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,59 +1,101 @@
 from django import forms
 
-from django.forms import (
-    ModelForm, TextInput, EmailField,
-    EmailInput, CharField, PasswordInput,
-    )
-
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm, ReadOnlyPasswordHashField, )
 
 from django.core.exceptions import ValidationError
 
-
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email', )
+from .models import User
 
 
-class SignUpForm(UserCreationForm):
-    username = CharField(label='Username',
-        widget=TextInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    email = EmailField(label='Email',
-        widget=EmailInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    password1 = CharField(label='Password',
-        widget=PasswordInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
-    password2 = CharField(label='Confirm password',
-        widget=PasswordInput(
-            attrs={'class': 'form-control',
-                   'required': True,
-                   }))
+class UserCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation',
+                                widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('email', )
 
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        qs = User.objects.filter(username=username)
-        if qs.exists():
-            raise ValidationError("Username already in use!")
-        return username
+    def clean_email(self):
+        # Check if there exists an email in db
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already in use!")
+        return email
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match!")
+        return password2
 
     def save(self, commit=True):
-        instance = super().save(commit=False)
-        instance.first_name = self.cleaned_data.get('username', '').capitalize()
-        instance.email = self.cleaned_data.get('email', '')
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
         if commit:
-            instance.save()
-        return instance
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """
+    A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial['password']
+
+# class SignUpForm(UserCreationForm):
+#     email = EmailField(label='Email',
+#         widget=EmailInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
+#     password1 = CharField(label='Password',
+#         widget=PasswordInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
+#     password2 = CharField(label='Confirm password',
+#         widget=PasswordInput(
+#             attrs={'class': 'form-control',
+#                    'required': True,
+#                    }))
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password1', 'password2', )
+
+#     def clean_username(self):
+#         username = self.cleaned_data.get('username')
+#         qs = User.objects.filter(username=username)
+#         if qs.exists():
+#             raise ValidationError("Username already in use!")
+#         return username
+
+#     def save(self, commit=True):
+#         instance = super().save(commit=False)
+#         instance.first_name = self.cleaned_data.get('username', '').capitalize()
+#         instance.email = self.cleaned_data.get('email', '')
+#         if commit:
+#             instance.save()
+#         return instance
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+class UserManager(BaseUserManager):
+    """
+    Custom user manager
+    """
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        # Normalizes email address by lowercasing the domain portion.
+        user = self.model(email=self.normalize_email(email), )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(email,
+                                password=password, )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    """
+    Custom user model. User is identified by email, instead of username.
+    """
+    email           = models.EmailField(unique=True,
+                                        verbose_name='email address',
+                                        max_length=255)
+    is_active       = models.BooleanField(default=True)
+    is_admin        = models.BooleanField(default=False)
+
+    objects             = UserManager()
+
+    USERNAME_FIELD  = 'email'
+    REQUIRED_FIELDS = [] # USERNAME_FIELD and password are always prompted for
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
