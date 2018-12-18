4108e33fba197c92f671bd5cedc0b24c90f939ba
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-18 20:50:38 +0000

    CHANGELOG
    1. Updated create/change form names
    2. Added additional 'name' field

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,33 +3,33 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
-from .forms import UserChangeForm, UserCreationForm
+from .forms import AdminChangeForm, AdminCreationForm
 
 
 class UserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
-    form = UserChangeForm
-    add_form = UserCreationForm
+    form = AdminChangeForm
+    add_form = AdminCreationForm
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
     list_display = ('email', 'is_admin', )
-    list_filter = ('is_admin',)
+    list_filter = ('is_admin', )
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        (None, {'fields': ('email', 'name', 'password')}),
+        ('Permissions', {'fields': ('is_admin', )}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
+            'classes': ('wide', ),
             'fields': ('email', 'password1', 'password2')}
         ),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+    search_fields = ('email', 'name', )
+    ordering = ('email', )
     filter_horizontal = ()
 
 # Now register the new UserAdmin...
