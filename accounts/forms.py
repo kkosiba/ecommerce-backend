6ec00892521864c80b8d3a8d7b3a75efecf49236
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-20 15:41:15 +0000

    CHANGELOG
    1. Reorganized code (split into user related and admin related forms)
    2. Added UserUpdateForm
    3. Added basic LoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,8 +7,9 @@ from django.core.exceptions import ValidationError
 
 from .models import User
 
+""" SUPERUSER FORMS """
 
-class UserCreationForm(forms.ModelForm):
+class AdminCreationForm(forms.ModelForm):
     """
     A form for creating new users. Includes all the required
     fields, plus a repeated password.
@@ -20,7 +21,7 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('email', )
+        fields = ('name', 'email')
 
     def clean_email(self):
         # Check if there exists an email in db
@@ -46,7 +47,7 @@ class UserCreationForm(forms.ModelForm):
         return user
 
 
-class UserChangeForm(forms.ModelForm):
+class AdminChangeForm(forms.ModelForm):
     """
     A form for updating users. Includes all the fields on
     the user, but replaces the password field with admin's
@@ -56,7 +57,7 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'is_active', 'is_admin')
+        fields = ('email', 'name', 'password', 'is_active', 'is_admin')
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
@@ -64,38 +65,83 @@ class UserChangeForm(forms.ModelForm):
         # field does not have access to the initial value
         return self.initial['password']
 
-# class SignUpForm(UserCreationForm):
-#     email = EmailField(label='Email',
-#         widget=EmailInput(
-#             attrs={'class': 'form-control',
-#                    'required': True,
-#                    }))
-#     password1 = CharField(label='Password',
-#         widget=PasswordInput(
-#             attrs={'class': 'form-control',
-#                    'required': True,
-#                    }))
-#     password2 = CharField(label='Confirm password',
-#         widget=PasswordInput(
-#             attrs={'class': 'form-control',
-#                    'required': True,
-#                    }))
+""" REGULAR USER FORMS """
 
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password1', 'password2', )
+class UserCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    name        = forms.CharField(label='Name',
+                                  required=False,
+                                  widget=forms.TextInput(
+                                    attrs={'class': 'form-control', }))
+    email       = forms.CharField(label='Email',
+                                  required=True,
+                                  widget=forms.EmailInput(
+                                    attrs={'class': 'form-control', }))
+    password1   = forms.CharField(label='Password',
+                                  widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control', }))
+    password2   = forms.CharField(label='Password confirmation',
+                                  widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control', }))
 
-#     def clean_username(self):
-#         username = self.cleaned_data.get('username')
-#         qs = User.objects.filter(username=username)
-#         if qs.exists():
-#             raise ValidationError("Username already in use!")
-#         return username
+    class Meta:
+        model = User
+        fields = ('name', 'email')
 
-#     def save(self, commit=True):
-#         instance = super().save(commit=False)
-#         instance.first_name = self.cleaned_data.get('username', '').capitalize()
-#         instance.email = self.cleaned_data.get('email', '')
-#         if commit:
-#             instance.save()
-#         return instance
+    def clean_email(self):
+        # Check if there exists an email in db
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already in use!")
+        return email
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match!")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.name = self.cleaned_data.get('name', '').capitalize()
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = False # email validation will flip this flag
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    """
+    Form used to update email and name
+    """
+    email = forms.EmailField(label='Email',
+                             required=True,
+                             widget=forms.EmailInput(
+                                attrs={'class': 'form-control', }))
+
+    name = forms.CharField(label='Name',
+                           required=False,
+                           widget=forms.TextInput(
+                                attrs={'class': 'form-control', }))
+
+    class Meta:
+        model = User
+        fields = ('email', 'name')
+
+
+class LoginForm(forms.Form):
+    email       = forms.EmailField(label='Email',
+                                   required=True,
+                                   widget=forms.EmailInput(
+                                        attrs={'class': 'form-control', }))
+    password    = forms.CharField(widget=forms.PasswordInput(
+                                        attrs={'class': 'form-control', }))
+
+    # to be implemented...
