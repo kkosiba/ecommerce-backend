4f744bab8c2cde4c62a2205e7909ee9b9c27ef20
Kamil Kosiba <kamil.kosiba@outlook.com>
2018-12-20 15:46:40 +0000

    Customized standard Django registration views; reorganized code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,48 +1,76 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+
+from django.contrib.auth.views import (
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView, )
+
 from django.urls import reverse_lazy
 
 
-from .views import SignUp
+from .views import (
+    AccountDetailView,
+    AccountCreateView,
+    AccountUpdateView,
+    LoginView, )
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', 
-        auth_views.LoginView.as_view(), 
-        name='login'),
+    path('register/',
+         AccountCreateView.as_view(),
+         name='register'),
 
-    path('logout/', 
-        auth_views.LogoutView.as_view(), 
-        name='logout'),
-    
+    path('login/',
+         LoginView.as_view(),
+         name='login'),
+
+    path('logout/',
+         LogoutView.as_view(
+            template_name='accounts/account_logout.html'),
+         name='logout'),
+
+    path('details/',
+         AccountDetailView.as_view(),
+         name='details'),
+
     path('password_change/',
-        auth_views.PasswordChangeView.as_view(
+         PasswordChangeView.as_view(
+            template_name='accounts/password_change_form.html',
             success_url=reverse_lazy('accounts:password_change_done')),
-        name='password_change'),
-    
+         name='password_change'),
+
     path('password_change_done/',
-        auth_views.PasswordChangeDoneView.as_view(),
-        name='password_change_done'),
-    
+         PasswordChangeDoneView.as_view(
+            template_name='accounts/password_change_done.html'),
+         name='password_change_done'),
+
     path('password_reset/',
-        auth_views.PasswordResetView.as_view(
+         PasswordResetView.as_view(
+            template_name='accounts/password_reset_form.html',
+            email_template_name='accounts/password_reset_email.html',
+            subject_template_name='accounts/password_reset_subject.txt',
             success_url=reverse_lazy('accounts:password_reset_done')),
-        name='password_reset'),
+         name='password_reset'),
 
     path('password_reset_done/',
-        auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done'),
+         PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
 
     path('password_reset_confirm/',
-        auth_views.PasswordResetConfirmView.as_view(
+         PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html',
             success_url=reverse_lazy('accounts:password_reset_complete')),
-        name='password_reset_done'),
+         name='password_reset_done'),
 
     path('password_reset_complete/',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete'),
-
-    path('signup/', SignUp.as_view(), name='signup'),
+         PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
