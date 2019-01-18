c6bf9bae3cb1550fc9b3c0d22b550a6231eaced9
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-01-18 19:17:37 +0000

    Registered Product and Category in Admin

--- backend/src/products/admin.py
+++ backend/src/products/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
+
 
 class ProductAdmin(admin.ModelAdmin):
-      list_display = ('name', 'description', )
+    list_display = ('name', 'price', 'quantity', 'featured', )
+    list_filter = ('name', 'price', 'quantity', 'featured', )
+    list_editable = ('price', 'quantity', )
 
-# Register your models here.
+    # sets up slug to be generated from product name
+    prepopulated_fields = {'slug': ('name', )}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_display_links = ('name', )
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
