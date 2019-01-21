4724ca1fa95b606ca4dddec60a5496a94c2436a4
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-01-21 20:57:21 +0000

    Added CategorySerializer; changed lookup field in ProductSerializer

--- backend/src/products/api/serializers.py
+++ backend/src/products/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
-from products.models import Product
+from products.models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', )
 
 class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'picture', )
+        fields = ('id', 'name', 'price', 'quantity',
+                  'featured', 'description', 'picture', 'slug', )
+        lookup_field = 'slug'
+
+    
