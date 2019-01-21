d5719d3f561c655742a311b5a5bd66471335ed3c
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-01-21 20:56:18 +0000

    Added CategoryViewSet; changed lookup field in ProductViewSet

--- backend/src/products/api/views.py
+++ backend/src/products/api/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
-from .serializers import ProductSerializer
-from products.models import Product
+from .serializers import ProductSerializer, CategorySerializer
+from products.models import Product, Category
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     # permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = ProductSerializer
-    
+    lookup_field = 'slug'
