9a8b032ee546563e24154c3713ebc3980a09c661
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-01-21 20:51:28 +0000

    Extended Product model with additional fields; added Category model

--- backend/src/products/models.py
+++ backend/src/products/models.py
@@ -1,10 +1,69 @@
 from django.db import models
+from django.urls import reverse
+
+# tags
+from taggit.managers import TaggableManager
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.CharField(max_length=300)
+class Category(models.Model):
+    name = models.CharField(max_length=100,
+                            unique=True)
+    # parent = models.ForeignKey('self',
+    #                            on_delete=models.CASCADE,
+    #                            null=True,
+    #                            blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('products:category', kwargs={'name': self.name})
+
+
+class Product(models.Model):
+    category = models.ManyToManyField(Category,
+                                      blank=False)
+
+    tags = TaggableManager(blank=True)  # tags mechanism
+
+    name = models.CharField(max_length=150)
+
+    slug = models.SlugField(max_length=200)
+
+    description = models.TextField(max_length=500,
+                                   default='Empty description.')
+
+    picture = models.ImageField(upload_to='products/images',
+                                null=True,
+                                blank=True)
+
+    price = models.DecimalField(decimal_places=2,
+                                max_digits=20,
+                                default=0)
+
+    # available quantity of given product
+    quantity = models.IntegerField(default=10)
+
+    # is product featured?
+    featured = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('products:products_detail', kwargs={'slug': self.slug})
+
+    @property
+    def is_featured(self):
+        return self.featured
+
+    @property
+    def is_available(self):
+        return self.quantity > 0
     
