8cf4b1f31518cfb2613155703090e74f91972aa8
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-01-22 20:36:09 +0000

    Split main App into components; added basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+// import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="content">
+        <Navbar />
+        <main className="container content py-4 bg-light">
+          <Switch>
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route path="/product/:slug" component={ProductDetails} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/about" component={About} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+        <Footer />
       </div>
     );
   }
