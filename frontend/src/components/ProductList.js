a01a75109228f3f5508afcf7453a469b8f523e8b
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-01-28 14:58:19 +0000

    CHANGELOG:
    1. Connected ProductList to redux store
    2. Added redux-thunk to redux-store middleware

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,51 +1,36 @@
 import React, { Component } from "react";
-import axios from "axios";
 import Product from "./Product";
 
-export default class ProductList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: []
-    };
-  }
+import { connect } from "react-redux";
+import { fetchProducts } from "../actions";
+
+const mapStateToProps = state => {
+  return { products: state.products };
+};
+
+class ConnectedProductList extends Component {
 
   componentDidMount() {
-    this.refreshProductList();
+    this.props.dispatch(fetchProducts());
   }
 
-  refreshProductList = () => {
-    axios
-      .get("http://localhost:8000/api/products/")
-      .then(res => this.setState({ products: res.data }))
-      .catch(err => console.log(err));
-  };
-
-  renderProductList = () => {
-    const products = this.state.products;
-    return (
-      <ul className="item-grid list-unstyled mb-4">
-        {products.map(item => (
-          <li key={item.id}>
-            <Product 
-              name={item.name}
-              slug={item.slug}
-              description={item.description}
-              picture={item.picture}
-              price={item.price}
-            />
-          </li>
-        ))}
-      </ul>
-    )
-  };
-
   render() {
+    const { products } = this.props;
     return (
       <React.Fragment>
         <h1>Product List</h1>
-        {this.renderProductList()}
+        <ul className="item-grid list-unstyled mb-4">
+          {products.map(item => (
+            <li key={item.id}>
+              <Product item={item} />
+            </li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
 }
+
+const ProductList = connect(mapStateToProps)(ConnectedProductList)
+
+export default ProductList;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,18 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
+// import { loadState, saveState } from "./localStorage";
 
-const store = createStore(rootReducer);
+// const persistedState = loadState();
+
+const store = createStore(
+  rootReducer,
+  // persistedState,
+  applyMiddleware(thunk)
+);
+
+// store.subscribe(() => {
+//   saveState(store.getState());
+// });
 
 export default store;
