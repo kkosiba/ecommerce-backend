5f0ff2280b8fbe9291049651b32f7cd91181254b
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-04 20:50:06 +0000

    Moved authentication components to Authentication directory

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../store/actions";
 
+import { MDBInput } from "mdbreact";
+
 const mapStateToProps = state => {
   return {
     loading: state.loading,
@@ -33,36 +35,32 @@ class Login extends Component {
 
   render() {
     return (
-      <form>
-        <fieldset>
-          <legend>Login</legend>
-          <p>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              onChange={e => this.setState({ username: e.target.value })}
-            />
-          </p>
-          <p>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-          </p>
-          <p>
-            <button type="submit" onClick={e => this.handleSubmit()}>
-              Login
-            </button>
-          </p>
-
-          <p>
+      <React.Fragment>
+        <div className="form-group mx-auto card p-3 w-50">
+          <h3 className="text-center font-weight-bold">Login</h3>
+          <MDBInput
+            label="Username"
+            type="text"
+            id="username"
+            onChange={e => this.setState({ username: e.target.value })}
+          />
+          <MDBInput
+            label="Password"
+            type="password"
+            id="password"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <button
+            type="submit"
+            className="btn btn-primary w-25 mx-auto"
+            onClick={e => this.handleSubmit()}>
+            Login
+          </button>
+          <p className="text-center pt-2">
             Don't have an account? <Link to="/register">Register</Link>
           </p>
-        </fieldset>
-      </form>
+        </div>
+      </React.Fragment>
     );
   }
 }
