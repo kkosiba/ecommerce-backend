ddf07560514d1591678bf0935e17df4fc7e8b0e4
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-09 21:54:06 +0000

    Redesigned Navbar component; added jwt-decode to get username from token

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,82 +1,91 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import SearchForm from "./SearchForm";
+import jwt_decode from "jwt-decode"; // for decoding JWT tokens
+
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse
+} from "mdbreact";
 
 class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
   cartItemCount() {
     let result = 0;
     const { cart } = this.props;
-    cart.map(item => result += item.quantity);
+    cart.map(item => (result += item.quantity));
     return result;
   }
 
   render() {
+    let decoded_token = undefined;
+    if (this.props.token) {
+      decoded_token = jwt_decode(this.props.token);
+    }
+
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container px-1">
-
-          <Link to="/">
-            <span className="navbar-brand font-weight-bold">
-              {/*<img src="clover.png" alt="logo" />*/}CloverPlants
-            </span>
-          </Link>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link font-weight-bold">
-                  Products
-                </Link>
-              </li>
-              <li className="nav-item pl-2">
+      <MDBNavbar color="black" dark expand="md">
+        <div className="container">
+          <MDBNavbarBrand>
+            <strong className="white-text">
+              <Link to="/">CloverPlants</Link>
+            </strong>
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav left>
+              <MDBNavItem>
                 <SearchForm />
-              </li>
-            </ul>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to="/cart" className="nav-link font-weight-bold">
-                  <i className="fas fa-shopping-cart" /> Cart ({this.cartItemCount()})
-                </Link>
-              </li>
-              <li className="nav-item pl-4">
-                {
-                  this.props.isAuthenticated ?
+              </MDBNavItem>
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+              <MDBNavItem className="mr-2 pt-1">
+                <MDBNavLink to="/cart">
+                  <i className="fas fa-shopping-cart" />
+                  <strong>Cart</strong> ({this.cartItemCount()})
+                </MDBNavLink>
+              </MDBNavItem>
+
+              <MDBNavItem>
+                {this.props.isAuthenticated ? (
                   <div class="btn-group btn-group">
                     <Link to="/profile" className="btn btn-success">
-                      User <i class="fas fa-user" />
+                      {decoded_token.username} <i class="fas fa-user" />
                     </Link>
-                    <button className="btn btn-warning" onClick={this.props.logout}>
+                    <button
+                      className="btn btn-warning"
+                      onClick={this.props.logout}
+                    >
                       Logout <i class="fas fa-sign-out-alt" />
                     </button>
                   </div>
-                  :
+                ) : (
                   <div class="btn-group btn-group">
                     <Link to="/login" className="btn btn-warning">
                       Login <i class="fas fa-sign-in-alt" />
                     </Link>
-                    <Link to="/register" className="btn btn-outline-secondary">
+                    <Link to="/register" className="btn btn-info">
                       Register <i class="fas fa-user-plus" />
                     </Link>
                   </div>
-                }
-              </li>
-            </ul>
-          </div>
+                )}
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
         </div>
-      </nav>
+      </MDBNavbar>
     );
   }
 }
