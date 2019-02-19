586b477f486043bc6d7ace61e19b7f70c7b3de1b
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-19 18:55:53 +0000

    Fixed inCart method; syntax corrections

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { addProductToCart,
-         removeProductFromCart } from "../store/actions";
+import { addProductToCart, removeProductFromCart } from "../store/actions";
 
 const mapStateToProps = state => {
-  return state;
+  return { cart: state.store.cart };
 };
 
 function mapDispatchToProps(dispatch) {
   return {
     addProductToCart: item => dispatch(addProductToCart(item, 1)),
-    removeProductFromCart: item => dispatch(removeProductFromCart(item)),
+    removeProductFromCart: item => dispatch(removeProductFromCart(item))
   };
 }
 
 class Product extends Component {
-
   inCart = () => {
     const { item, cart } = this.props;
-    return cart.some(e => e.id === item.id);
+    const res = cart.items.find(e => e.id === item.id);
+    if (res) {
+      return res.quantity;
+    } else {
+      return 0;
+    }
   };
 
   render() {
@@ -29,27 +32,36 @@ class Product extends Component {
     return (
       <div className="card rounded-0">
         <Link to={`/product/${item.slug}`} className="text-dark">
-        <img className="card-img-top rounded-0" src={item.picture} alt={item.name} />
+          <img
+            className="card-img-top rounded-0"
+            src={item.picture}
+            alt={item.name}
+          />
         </Link>
         <div className="d-flex flex-column card-body">
           <h5 className="card-text">{item.name}</h5>
-          <p className="card-text"><strong>Price: £{item.price}</strong></p>
+          <p className="card-text">
+            <strong>Price: £{item.price}</strong>
+          </p>
           <div className="d-flex flex-row justify-content-center">
             <button
               type="submit"
               className="btn btn-success"
               onClick={() => this.props.addProductToCart(item)}
             >
-              <i class="fas fa-cart-plus"></i>
+              <i className="fas fa-cart-plus" />
             </button>
-            {this.inCart() ?
+            {this.inCart() > 0 ? (
               <button
                 type="submit"
                 className="btn btn-danger ml-1"
                 onClick={() => this.props.removeProductFromCart(item)}
               >
-              <i class="far fa-trash-alt"></i>
-              </button>:""}
+                <i className="far fa-trash-alt" />
+              </button>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
@@ -57,4 +69,7 @@ class Product extends Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Product);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Product);
