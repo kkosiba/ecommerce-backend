e2b6d9596aed41bc627e1a6ea754691da376063d
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-21 18:19:33 +0000

    Updated accounts app

--- backend/src/accounts/admin.py
+++ backend/src/accounts/admin.py
@@ -10,6 +10,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'name']
+    list_display = ('email', 'first_name', 'last_name', )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- backend/src/accounts/models.py
+++ backend/src/accounts/models.py
@@ -3,7 +3,5 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    name = models.CharField(max_length=255)
-
     def __str__(self):
         return self.email
