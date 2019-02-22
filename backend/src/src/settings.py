7fd8f722fa91c928e7d31208161f4c8e328e0202
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-22 11:18:44 +0000

    Added STATIC_ROOT

--- backend/src/src/settings.py
+++ backend/src/src/settings.py
@@ -157,7 +157,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 
 # Media files
