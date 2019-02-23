5047dc1bfda65946de1e4907088423ff6c3e5456
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-23 20:05:10 +0000

    Updated Default route

--- frontend/src/components/Default.js
+++ frontend/src/components/Default.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>Page not found.</h3>
-      </div>
+      <React.Fragment>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <h1 className="font-weight-bold">Page not found.</h1>
+        </div>
+      </React.Fragment>
     );
   }
 }
