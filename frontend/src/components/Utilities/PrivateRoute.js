27329d3724781a77a831cfdf650730721347d340
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-02-23 20:06:40 +0000

    Removed redundant code from PrivateRoute component

--- frontend/src/components/Utilities/PrivateRoute.js
+++ frontend/src/components/Utilities/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, cart, ...rest }) => (
+const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
       localStorage.getItem("token") ? (
-        <Component cart={cart} {...props} />
+        <Component {...props} />
       ) : (
         <Redirect
           to={{
