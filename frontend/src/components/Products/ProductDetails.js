1134f56267ffce5a6edd0dad520fac0fba0bf6ca
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-04 19:23:13 +0000

    Added FontAwesome; replaced redundant state attribute with productExists

--- frontend/src/components/Products/ProductDetails.js
+++ frontend/src/components/Products/ProductDetails.js
@@ -6,6 +6,8 @@ import { connect } from "react-redux";
 import * as actions from "../../store/actions";
 
 import { API_PATH } from "../../backend_url";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Default from "../Default";
 
 const mapStateToProps = state => {
   return { cartItems: state.store.cart.items };
@@ -23,7 +25,7 @@ class ProductDetails extends Component {
     super(props);
     this.state = {
       product: {},
-      quantity: 0,
+      productExists: false,
       selectValue: 1
     };
     this.handleChange = this.handleChange.bind(this);
@@ -35,8 +37,8 @@ class ProductDetails extends Component {
     } = this.props;
     axios
       .get(`${API_PATH}products/${params.slug}`)
-      .then(res => this.setState({ product: res.data }))
-      .catch(err => console.log(err));
+      .then(res => this.setState({ product: res.data, productExists: true }))
+      .catch(err => undefined); // ignore error
   }
 
   inCart = () => {
@@ -72,100 +74,105 @@ class ProductDetails extends Component {
   };
 
   render() {
-    const { product } = this.state;
-    const productQuantityRange = this.quantityRange(product);
-
-    return (
-      <div className="row">
-        <div className="col-md-6">
-          <div className="card rounded-0">
-            <img
-              className="card-img-top rounded-0"
-              src={product.picture}
-              alt={product.name}
-            />
+    const { product, productExists } = this.state;
+    
+    if (!productExists) {
+      return <Default />;
+    } else {
+      const productQuantityRange = this.quantityRange(product);
+  
+      return (
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card rounded-0">
+              <img
+                className="card-img-top rounded-0"
+                src={product.picture}
+                alt={product.name}
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-md-6">
-          <div className="card rounded-0">
-            <div className="card-body">
-              <h3>
-                <strong>{product.name}</strong>
-              </h3>
-              <p className="card-text pt-2">{product.description}</p>
-              <p className="card-text pt-2">
-                <strong>Price:</strong> £{product.price}
-              </p>
-              <p className="card-text pt-2">
-                <strong>Availability:</strong>
-                {product.quantity > 0 ? (
-                    product.quantity > 20 ? " Plenty in stock!" : " Just a few left!"
-                ) : (
-                  " Out of stock"
-                )}
-              </p>
-
-              <hr />
-
-              <div className="input-group justify-content-center">
-                <div className="input-group-prepend">
-                  <button className="btn btn-info" type="button">
-                    <Link to="/" className="text-white">
-                      <i className="far fa-list-alt" /> All products
-                    </Link>
-                  </button>
-                </div>
-                {product.quantity === 0 ? (
-                  <div className="input-group-append">
-                    <button className="btn btn-success" disabled>
-                      <i className="fas fa-cart-plus" />
+          <div className="col-md-6">
+            <div className="card rounded-0">
+              <div className="card-body">
+                <h3>
+                  <strong>{product.name}</strong>
+                </h3>
+                <p className="card-text pt-2">{product.description}</p>
+                <p className="card-text pt-2">
+                  <strong>Price:</strong> £{product.price}
+                </p>
+                <p className="card-text pt-2">
+                  <strong>Availability:</strong>
+                  {product.quantity > 0 ? (
+                      product.quantity > 20 ? " Plenty in stock!" : " Just a few left!"
+                  ) : (
+                    " Out of stock"
+                  )}
+                </p>
+  
+                <hr />
+  
+                <div className="input-group justify-content-center">
+                  <div className="input-group-prepend">
+                    <button className="btn btn-info" type="button">
+                      <Link to="/" className="text-white">
+                        <FontAwesomeIcon icon="list-alt" /> Products
+                      </Link>
                     </button>
                   </div>
-                ) : (
-                  <div className="input-group-append">
-                    <select
-                      className="custom-select"
-                      value={this.state.selectValue}
-                      onChange={this.handleChange}
-                    >
-                      {productQuantityRange
-                        .slice(0, 10) // add no more than 10 items to cart regardless of the available amount
-                        .map(item => (
-                          <option value={item} key={item}>
-                            {item}
-                          </option>
-                        ))}
-                    </select>
-                    <button
-                      type="submit"
-                      className="btn btn-success"
-                      onClick={() =>
-                        this.addMultiple(product, this.state.selectValue)
-                      }
-                    >
-                      <i className="fas fa-cart-plus" />
-                    </button>
-                    {this.inCart() > 0 ? (
+                  {product.quantity === 0 ? (
+                    <div className="input-group-append">
+                      <button className="btn btn-success" disabled>
+                        <FontAwesomeIcon icon="cart-plus" />
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="input-group-append">
+                      <select
+                        className="custom-select"
+                        value={this.state.selectValue}
+                        onChange={this.handleChange}
+                      >
+                        {productQuantityRange
+                          .slice(0, 10) // add no more than 10 items to cart regardless of the available amount
+                          .map(item => (
+                            <option value={item} key={item}>
+                              {item}
+                            </option>
+                          ))}
+                      </select>
                       <button
-                        type="button"
-                        className="btn btn-danger"
+                        type="submit"
+                        className="btn btn-success"
                         onClick={() =>
-                          this.props.removeProductFromCart(product)
+                          this.addMultiple(product, this.state.selectValue)
                         }
                       >
-                        <i className="far fa-trash-alt" />
+                        <FontAwesomeIcon icon="cart-plus" />
                       </button>
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                )}
+                      {this.inCart() > 0 ? (
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          onClick={() =>
+                            this.props.removeProductFromCart(product)
+                          }
+                        >
+                          <FontAwesomeIcon icon="trash-alt" />
+                        </button>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
