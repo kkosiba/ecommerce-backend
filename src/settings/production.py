1b5a73c10b4299224373d157b48c6bb06dd64eab
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-25 19:09:35 +0000

    Tuned AWS S3 settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -24,6 +24,8 @@ STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY", "")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = "src.storage_backends.StaticStorage"
+DEFAULT_FILE_STORAGE = "src.storage_backends.MediaStorage"
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
@@ -32,11 +34,10 @@ AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
-AWS_LOCATION = "static"
-
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-DEFAULT_FILE_STORAGE = "src.storage_backends.MediaStorage"
+AWS_PRELOAD_METADATA = True
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, "static")
+ADMIN_MEDIA_PREFIX = "https://%s/%s/admin/" % (AWS_S3_CUSTOM_DOMAIN, "static")
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, "media")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
--- src/storage_backends.py
+++ src/storage_backends.py
@@ -3,3 +3,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
+
+class StaticStorage(S3Boto3Storage):
+    location = "static"
+    file_overwrite = False
