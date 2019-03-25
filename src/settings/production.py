86a63a47bb47a160e991786ec661da5059de10e4
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-25 16:24:48 +0000

    Added STATIC_* and MEDIA_* settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -10,6 +10,12 @@ DEBUG = os.environ.get("DEBUG", "")
 
 ALLOWED_HOSTS = ["https://ecommerce-backend-django.herokuapp.com/", ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", "")
 STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY", "")
 
