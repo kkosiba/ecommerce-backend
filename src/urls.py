ad7325140385cc705ee3066bbca2a45dd137af53
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-06-02 20:52:16 +0100

    Added API root path

--- src/urls.py
+++ src/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='templates/base.html'), name='index'),
+    path('', TemplateView.as_view(template_name='templates/base.html'), name='index'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
