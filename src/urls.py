9fb330f28fa360b3f4bd580c576286e43677fca9
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-06-02 21:16:54 +0100

    Changed / path

--- src/urls.py
+++ src/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='templates/base.html'), name='index'),
+    path('', TemplateView.as_view(template_name='base.html'), name='index'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
