ccf83c8a30b1da40043c16cabd0eeb7f3f0e2fde
Kamil Kosiba <kamil.kosiba@outlook.com>
2025-01-14 18:41:39 +0000

    Add script to automatically update dependencies using pip-tools
    Also added pyproject.toml containing project details, including
    dependencies.

---
+++ update_requirements.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# This script updates requirements *.txt files in requirements directory
+# based on the dependency definitions in the pyproject.toml file. It needs
+# to be executed from the repository root.
+
+function cleanup() {
+  rm -rf *.egg_info
+}
+
+trap cleanup EXIT
+
+# Layered dependencies
+python -m piptools compile \
+  --upgrade \
+  --resolver backtracking \
+  --extra base \
+  --output-file requirements/base.txt \
+  pyproject.toml
+
+python -m piptools compile \
+  --upgrade \
+  --resolver backtracking \
+  --extra base \
+  --extra development \
+  --output-file requirements/development.txt \
+  pyproject.toml
+
+python -m piptools compile \
+  --upgrade \
+  --resolver backtracking \
+  --extra base \
+  --extra production \
+  --output-file requirements/production.txt \
+  pyproject.toml
